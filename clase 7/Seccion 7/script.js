aec6a89cc29dbcba6df51796a6a532fb3db664c3
NaruCrow <135269746+NaruCrow@users.noreply.github.com>
2023-06-01 16:43:07 -0300

    Initial commit

---
+++ clase 7/Seccion 7/script.js
@@ -0,0 +1,3 @@
+document.querySelector("button.button-menu-toggle").addEventListener("click", function () {
+    document.querySelector("#menu-links").classList.toggle("show"); // Agregamos la clase "show" al elemento de los enlaces
+});
